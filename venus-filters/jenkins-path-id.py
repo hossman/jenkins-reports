96599e35afb86c4e61b9331f38ed3cb95ec375d0
Chris Hostetter <hossman_github@fucit.org>
2017-10-18 16:14:15 -0700

    tweak our jenkins specific filter to also create a url.txt file for each build, so we can start writting the post processing scripts to download all the raw data

--- venus-filters/jenkins-path-id.py
+++ venus-filters/jenkins-path-id.py
@@ -15,13 +15,15 @@
 
 """
 Add a 'path-id' to each entry based on the original 'link' and what we know about jenkins build URLs
+and then create a directory with that path-id where we create a url.txt (if it doesn't already exist)
 
 NOTE: Due to laziness, this depends on having 'ignore_in_feed = id' configured   (Since it's 
-      neccessary for jenkins feeds to not collied anyway) as well as each feed having an 'id' configured
+      neccessary for jenkins feed entires to not conflict anyway) as well as each feed having an 
+      'id' configured
 
 """
 
-import re, sys, urlparse, xml.dom.minidom
+import os, re, sys, urlparse, xml.dom.minidom
 
 atomNS = 'http://www.w3.org/2005/Atom'
 planetNS = 'http://planet.intertwingly.net/'
@@ -29,18 +31,28 @@ planetNS = 'http://planet.intertwingly.net/'
 dom = xml.dom.minidom.parse(sys.stdin)
 entry = dom.documentElement
 
-# go get the source id
-source = entry.getElementsByTagNameNS(planetNS, 'id')[0].firstChild.nodeValue
+# go get the feed id & entry id (aka: entry URL)
+feed_id = entry.getElementsByTagNameNS(planetNS, 'id')[0].firstChild.nodeValue
+entry_id = entry.getElementsByTagNameNS(atomNS, 'id')[0].firstChild.nodeValue
 
-orig_id = entry.getElementsByTagNameNS(atomNS, 'id')[0].firstChild.nodeValue
+path_id_parts = entry_id.split('/')[-2:] # job name + build #
+path_id_parts.insert(0, feed_id) # prepend feed_id
+path_id = '/'.join(path_id_parts)
 
-id_parts = orig_id.split('/')[-2:] # job name + build #
-id_parts.insert(0, source)
-
+# add it to the DOM
 path_id_element = dom.createElementNS(planetNS, 'path-id')
 # fucking hack: https://stackoverflow.com/questions/863774/how-to-generate-xml-documents-with-namespaces-in-python
-path_id_element.setAttribute("xmlns", planetNS) 
+path_id_element.setAttribute('xmlns', planetNS) 
+path_id_element.appendChild(dom.createTextNode(path_id))
 entry.appendChild(path_id_element)
-path_id_element.appendChild(dom.createTextNode('/'.join(id_parts)))
 
+# go create the directory & url.txt if they don't exist
+url_file_name = '/'.join(('output', 'html', 'job-data', path_id, 'url.txt'));
+if not os.path.exists(os.path.dirname(url_file_name)):
+    os.makedirs(os.path.dirname(url_file_name))
+if not os.path.exists(url_file_name):
+    with open(url_file_name, "w") as f:
+        f.write(entry_id)
+
+# very last thing we do is write the updated DOM back to venus
 print entry.toxml('utf-8')

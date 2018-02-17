669fbe359a6dd1e6329ef971cb3dc7cfb17b9c47
Chris Hostetter <hossman_github@fucit.org>
2018-02-16 17:59:57 -0700

    generate some '*-failure-rates.csv' reports showing the ratio of failure/runs for all test methods and suites

---
+++ gen-failure-rates.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl -l
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#######
+
+use strict;
+use warnings;
+my $SUITES_ALREADY_SEEN_PER_FILE = {};
+my $RESULTS = {};
+
+sub add_run {
+    my ($class, $method, $run_inc, $fail_inc) = @_;
+    if (exists $RESULTS->{"$class,$method"}) {
+	$RESULTS->{"$class,$method"}->{"run"} += $run_inc;
+	$RESULTS->{"$class,$method"}->{"fail"} += $fail_inc;
+    } else {
+	$RESULTS->{"$class,$method"} = { "run" => $run_inc, 
+					 "fail" => $fail_inc };
+    }
+}
+
+
+while (<>) {
+    my ($file,$line) = split /:/;
+    my ($status, $class, $method) = (split(",", $line))[0,1,2];
+    my $run_inc = ("SKIP" eq $status ? 0 : 1);
+    my $fail_inc = ("FAIL" eq $status ? 1 : 0);
+    
+    if ('' ne $method) {
+	# if this is *not* a suite failure, record an extra psuedo-run for the suite this method is in...
+	# if and only if we haven't already done that for this file + suite combo
+	if (! exists $SUITES_ALREADY_SEEN_PER_FILE->{$class}) {
+	    $SUITES_ALREADY_SEEN_PER_FILE->{$class} = {};
+	}
+	if (! exists $SUITES_ALREADY_SEEN_PER_FILE->{$class}->{$file}) {
+	    $SUITES_ALREADY_SEEN_PER_FILE->{$class}->{$file} = 1;
+	    add_run($class, '', 1, 0);
+	}
+    } else {
+	# sanity check: suites should only be in the files if they were failures...
+	die "WTF: $_" unless 1 == $fail_inc;
+	# don't record the run, just the failure...
+	# (we already done a special recording of the run the first time we saw the class name)
+	$run_inc = 0;
+    }
+    add_run($class, $method, $run_inc, $fail_inc);
+}
+# we're done with this, let it GC...
+$SUITES_ALREADY_SEEN_PER_FILE = undef;
+
+while (my ($key, $value) = each %{$RESULTS}) {
+    # avoid divide by zero for tests that were skipped 100% of the time
+    $value->{"fail_rate"} = (0 < $value->{"run"}
+			     ? ($value->{"fail"} / $value->{"run"})
+			     : 0);
+} 
+foreach my $key (sort { $RESULTS->{$b}->{"fail_rate"} <=> $RESULTS->{$a}->{"fail_rate"}
+			# when the fail_rates are identical, secondary sort on the total number of failures
+			|| $RESULTS->{$b}->{"fail"} <=> $RESULTS->{$a}->{"fail"} } keys %{$RESULTS}) {
+    # don't bother reporting anything that never failed
+    if (0 < $RESULTS->{$key}->{"fail_rate"}) {
+	print join ",", $key, $RESULTS->{$key}->{"fail_rate"}, $RESULTS->{$key}->{"fail"}, $RESULTS->{$key}->{"run"};
+    }
+}
--- generate-test-summary-reports.sh
+++ generate-test-summary-reports.sh
@@ -20,31 +20,44 @@
 # Loops over all the recent '*.csv' files it can find (in job-data) and produces
 # some summary reports
 
-# NOTE: if changing any of the -mmin rules, make sure run.sh is updated to not prune files we care about
-
 #######
 
 echo "## Generating Summary Reports..."
 
 mkdir -p output/html/reports
 
-# TODO: a create a "recent-test-results.csv.gz"
-#  ... should looks like recent-method-failures.csv -- but with the FAIL/PASS still included
+### first up, get a list of all the test result files in the last 24hrs & 7 days
+### we want to ensure a consistent input list for every step after this
+#
+# NOTE: if changing any of the -mmin rules, make sure run.sh is updated to not prune files we care about
+find output/html/job-data/ -mmin -1440 -name \*.csv > output/html/reports/24hours-test-results.txt
+find output/html/job-data/ -mmin -10080 -name \*.csv > output/html/reports/7days-test-results.txt
 
 ### recent failures at method granularity - sorted, and including the directory for each
 # just past 24 hours
-find output/html/job-data/ -mmin -1440 -name \*.csv | xargs grep '^FAIL' | perl -nle 'my ($file,$line) = split /:/; $file =~ s{^.*/([^/]+/[^/]+/\d+/).*\.csv$}{$1}; $line = join(",", (split(",", $line))[1,2])  ; print "$line,$file"' | sort > output/html/reports/24hours-method-failures.csv
+cat output/html/reports/24hours-test-results.txt | xargs grep '^FAIL' | perl -nle 'my ($file,$line) = split /:/; $file =~ s{^.*/([^/]+/[^/]+/\d+/).*\.csv$}{$1}; $line = join(",", (split(",", $line))[1,2])  ; print "$line,$file"' | sort > output/html/reports/24hours-method-failures.csv
 # past 7 days
-find output/html/job-data/ -mmin -10080 -name \*.csv | xargs grep '^FAIL' | perl -nle 'my ($file,$line) = split /:/; $file =~ s{^.*/([^/]+/[^/]+/\d+/).*\.csv$}{$1}; $line = join(",", (split(",", $line))[1,2])  ; print "$line,$file"' | sort > output/html/reports/7days-method-failures.csv
+cat output/html/reports/7days-test-results.txt | xargs grep '^FAIL' | perl -nle 'my ($file,$line) = split /:/; $file =~ s{^.*/([^/]+/[^/]+/\d+/).*\.csv$}{$1}; $line = join(",", (split(",", $line))[1,2])  ; print "$line,$file"' | sort > output/html/reports/7days-method-failures.csv
 
 ### recent failures at a class level - summarized & sorted by by count
 # past 7 days
 perl -nle '$class = (split(",",$_))[0]; print $class' < output/html/reports/7days-method-failures.csv | uniq -c | sort -rn | perl -ple 's{^\s*(\d+)\s+(\S+)}{$1,$2};' > output/html/reports/7days-top-failing-classes.csv
 
+### recent failure *rates* at both a method and class level (class level listed as method '')
+# just past 24 hours
+cat output/html/reports/24hours-test-results.txt | xargs grep '^' | perl gen-failure-rates.pl > output/html/reports/24hours-failure-rates.csv
+# past 7 days 
+cat output/html/reports/7days-test-results.txt | xargs grep '^' | perl gen-failure-rates.pl > output/html/reports/7days-failure-rates.csv
+
+
 ### archive our reports based on the current date, overwritin any existing files with same name
 ### as the cron runs multiple times a day, this will result in the "last" run from each day being preserved
 mkdir -p output/html/reports/archive/daily
 mkdir -p output/html/reports/archive/weekly
+
 gzip -c output/html/reports/24hours-method-failures.csv > output/html/reports/archive/daily/`date -u +%F`.method-failures.csv.gz
 gzip -c output/html/reports/7days-method-failures.csv > output/html/reports/archive/weekly/`date -u +%G-%V`.method-failures.csv.gz
+
+gzip -c output/html/reports/24hours-failure-rates.csv > output/html/reports/archive/weekly/`date -u +%F`.failure-rates.csv.gz
+gzip -c output/html/reports/7days-failure-rates.csv > output/html/reports/archive/weekly/`date -u +%G-%V`.failure-rates.csv.gz
                                                  

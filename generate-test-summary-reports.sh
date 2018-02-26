97a943f9f85b64bc685d899e4ecee8c453953f65
Chris Hostetter <hossman_github@fucit.org>
2018-02-26 14:37:11 -0700

    sort jobs so more recent jobs are at the top of the list in our reports

--- generate-test-summary-reports.sh
+++ generate-test-summary-reports.sh
@@ -29,9 +29,12 @@ mkdir -p output/html/reports
 ### first up, get a list of all the test result files in the last 24hrs & 7 days
 ### we want to ensure a consistent input list for every step after this
 #
+# we want these in reverse sorted order, so newer files are at the top
+# that means that in later reports, recent jobs related to a failure will be listed first
+#
 # NOTE: if changing any of the -mmin rules, make sure run.sh is updated to not prune files we care about
-find output/html/job-data/ -mmin -1440 -name \*.csv > output/html/reports/24hours-test-results.txt
-find output/html/job-data/ -mmin -10080 -name \*.csv > output/html/reports/7days-test-results.txt
+find output/html/job-data/ -mmin -1440 -name \*.csv -printf "%T@\t%p\n" | sort -nr | perl -ple '$_=(split(/\s+/))[1];' > output/html/reports/24hours-test-results.txt
+find output/html/job-data/ -mmin -10080 -name \*.csv -printf "%T@\t%p\n" | sort -nr | perl -ple '$_=(split(/\s+/))[1];'> output/html/reports/7days-test-results.txt
 
 ### recent failures at method granularity - sorted, and including the directory for each
 # just past 24 hours
